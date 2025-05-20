9561cd3086386424e46a12a55d8dff26bab301fe
Aphreditto <108552825+aphreditto@users.noreply.github.com>
2025-05-20 17:15:19 +0000

    created simple web server in rust

---
+++ OMV/simple_server/target/debug/deps/simple_server-d903ca541c5906c5.d
@@ -0,0 +1,5 @@
+/workspaces/assorted-projects/OMV/simple_server/target/debug/deps/simple_server-d903ca541c5906c5: src/main.rs
+
+/workspaces/assorted-projects/OMV/simple_server/target/debug/deps/simple_server-d903ca541c5906c5.d: src/main.rs
+
+src/main.rs:
---
+++ OMV/simple_server/target/debug/simple_server.d
@@ -0,0 +1,1 @@
+/workspaces/assorted-projects/OMV/simple_server/target/debug/simple_server: /workspaces/assorted-projects/OMV/simple_server/src/main.rs
